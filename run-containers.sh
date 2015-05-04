d866882f80445a420752327521761db2c7d494d9
Marcin Jasion <marcin.jasion@gmail.com>
2015-05-05 01:41:11 +0200

    ssl

--- run-containers.sh
+++ run-containers.sh
@@ -5,8 +5,10 @@ docker run -d --name codepotpl-nginx \
     --link codepotpl-production:codepotpl-production \
     --link codepotbackendstaging_django_1:codepotbackendstaging_django_1 \
     -p 80:80 \
+    -p 443:443 \
     -p 8081:8081 \
     -p 8082:8082 \
     -v /var/log/nginx/:/var/log/nginx \
+    -v /etc/nginx/ssl:/ssl \
     -v /home/codepot/codepot-webclient-staging/dist:/registration \
     codepotpl-nginx:latest
