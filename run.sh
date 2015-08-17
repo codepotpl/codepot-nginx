662e17ef6c8db7366e11ed67cf3b1f04605145de
Marcin Jasion <marcin.jasion@gmail.com>
2015-08-17 16:18:48 +0200

    loadbalance

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ docker rm -f codepot-nginx-production
 docker run -d --name codepot-nginx-production \
     --link codepot-production:codepot-production \
     --link codepotbackendproduction_django_1:codepotbackendproduction_django_1 \
+    --link codepotbackendproduction_django_2:codepotbackendproduction_django_2 \
+    --link codepotbackendproduction_django_3:codepotbackendproduction_django_3 \
     -p 80:80 \
     -p 443:443 \
     -v /var/log/nginx/:/var/log/nginx \
