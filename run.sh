d50474900b327ae59ef0191da06ac798410a0617
Marcin Jasion <marcin.jasion@gmail.com>
2015-08-17 21:44:58 +0200

    first staging

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-docker rm -f codepot-nginx-staging
+docker rm -f codepot-nginx-aws
 
-docker run -d --name codepot-nginx-staging \
+docker run -d --name codepot-nginx-aws \
+    --link codepot-production:codepot-production \
     --link codepot-staging:codepot-staging \
     --link codepotbackendstaging_django_1:codepotbackendstaging_django_1 \
-    -p 8080:80 \
-    -p 8443:443 \
+    -p 80:80 \
+    -p 8080:8080 \
+    -p 8443:8443 \
     -v /var/log/nginx/:/var/log/nginx \
-    -v /etc/ssl/codepot:/ssl \
     -v /home/codepot/codepot-webclient-staging/dist:/registration \
     -v /var/files:/files \
     codepot-nginx-staging:latest
