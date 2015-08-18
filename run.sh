51bcbf28809e5a457a250895d9252037da355681
Marcin Jasion <marcin.jasion@gmail.com>
2015-08-19 00:40:27 +0200

    removed production

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #djangos=`docker ps --filter=name=codepotbackendproduction_django | grep django | sed 's/ \+/ /g' | cut -d ' ' -f 15-`
+#docker ps | grep django_ | sed 's/ \{2,\}/,/g' | cut -d ',' -f 7
 #links=""
 #i=1
 #for django in $djangos;  do
@@ -12,7 +13,6 @@
 
 docker rm -f codepot-nginx-production
 docker run -d --name codepot-nginx-production \
-    --link codepot-production:codepot-production \
     --link codepotbackendproduction_django_1:codepotbackendproduction_django_1 \
     --link codepotbackendproduction_django_2:codepotbackendproduction_django_2 \
     --link codepotbackendproduction_django_3:codepotbackendproduction_django_3 \
