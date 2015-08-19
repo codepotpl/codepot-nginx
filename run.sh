2369d360c5e0e051cc3e4f1696e2e34203ea0f3a
Marcin Jasion <marcin.jasion@gmail.com>
2015-08-19 21:10:13 +0200

    dynamic linking

--- run.sh
+++ run.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-#djangos=`docker ps --filter=name=codepotbackendproduction_django | grep django | sed 's/ \+/ /g' | cut -d ' ' -f 15-`
-#docker ps | grep django_ | sed 's/ \{2,\}/,/g' | cut -d ',' -f 7
-#links=""
-#i=1
-#for django in $djangos;  do
-#	links="$links --link codepotbackendproduction_django_$i:$django"
-#	i=$((i+1))
-#	echo $i
-#done
-#echo $links
+djangos=`docker ps | grep django_ | sed 's/ \{2,\}/,/g' | cut -d ',' -f 7`
+links=""
+i=1
+for django in $djangos;  do
+	links="$links --link $django:codepotbackendproduction_django_$i"
+	i=$((i+1))
+done
+echo $links
 
 docker rm -f codepot-nginx-production
 docker run -d --name codepot-nginx-production \
-    --link codepotbackendproduction_django_1:codepotbackendproduction_django_1 \
-    --link codepotbackendproduction_django_2:codepotbackendproduction_django_2 \
-    --link codepotbackendproduction_django_3:codepotbackendproduction_django_3 \
+    $links \
     -p 80:80 \
     -p 443:443 \
     -v /var/log/nginx/:/var/log/nginx \
@@ -23,3 +19,13 @@ docker run -d --name codepot-nginx-production \
     -v /etc/ssl/codepot:/ssl \
     codepot-nginx-production:latest
 
+#docker run -d --name codepot-nginx-production \
+#    --link codepotbackendproduction_django_1:codepotbackendproduction_django_1 \
+#    --link codepotbackendproduction_django_2:codepotbackendproduction_django_2 \
+#    --link codepotbackendproduction_django_3:codepotbackendproduction_django_3 \
+#    -p 80:80 \
+#    -p 443:443 \
+#    -v /var/log/nginx/:/var/log/nginx \
+#    -v /home/codepot/codepot-webclient-production/dist:/registration \
+#    -v /etc/ssl/codepot:/ssl \
+#    codepot-nginx-production:latest
