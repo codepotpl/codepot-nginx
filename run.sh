e2b1bcc3b8ef8ebf71a1f34506fb728eb754f93d
Marcin Jasion <marcin.jasion@gmail.com>
2015-08-23 23:32:24 +0200

    removed production site

--- run.sh
+++ run.sh
@@ -13,7 +13,6 @@ docker rm -f codepot-nginx-aws
 
 docker run -d --name codepot-nginx-aws \
     $links \
-    -p 80:80 \
     -p 8080:8080 \
     -p 8443:8443 \
     -v /var/log/nginx/:/var/log/nginx \
