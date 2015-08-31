8c8b240d5d9ebf0d94cb96763053b3ab5ff6c36a
Marcin Jasion <marcin.jasion@gmail.com>
2015-09-01 00:31:35 +0200

    survey

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ docker rm -f codepot-nginx-staging
 docker run -d --name codepot-nginx-staging \
     --link codepot-staging:codepot-staging \
     --link codepotbackendstaging_django_1:codepotbackendstaging_django_1 \
+    --link codepotsurveystaging_survey_1:codepotsurveystaging_survey_1 \
     -p 8080:8080 \
     -p 8443:8443 \
     -v /var/log/nginx/:/var/log/nginx \
